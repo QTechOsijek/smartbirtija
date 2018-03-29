2410aac2107b230ac1fd6c694f75555598d8d94b
Petar Susac <petar.susac@gmail.com>
2018-03-29 12:50:10 +0200

    Path change

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/dashboard', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
 })
 
