d4c388208f9239af312f73669941594abea46442
Petar Susac <petar.susac@gmail.com>
2018-03-29 12:06:13 +0200

    First commit

---
+++ client/src/App.js
@@ -0,0 +1,346 @@
+import React from 'react';
+import './App.css';
+
+const client = (function () {
+  function getOrders(success) {
+    return fetch('/api/orders', {
+      headers: {
+        Accept: 'application/json',
+      },
+    }).then(checkStatus)
+      .then(parseJSON)
+      .then(success);
+  }
+
+  function deleteOrder(data) {
+    return fetch('/api/orders', {
+      method: 'delete',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(checkStatus);
+  }
+  
+  function createOrder(data) {
+    return fetch('/api/orders', {
+      method: 'post',
+      body: JSON.stringify(data),
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    }).then(checkStatus);
+  }
+
+  function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response;
+    } else {
+      const error = new Error(`HTTP Error ${response.statusText}`);
+      error.status = response.statusText;
+      error.response = response;
+      console.log(error);
+      throw error;
+    }
+  }
+
+  function parseJSON(response) {
+    return response.json();
+  }
+
+  function millisecondsToHuman(ms) {
+    const seconds = Math.floor((ms / 1000) % 60);
+    const minutes = Math.floor((ms / 1000 / 60) % 60);
+    const hours = Math.floor(ms / 1000 / 60 / 60);
+
+    const humanized = [
+      pad(hours.toString(), 2),
+      pad(minutes.toString(), 2),
+      pad(seconds.toString(), 2),
+    ].join(':');
+
+    return humanized;
+  }
+
+  function pad(numberString, size) {
+    let padded = numberString;
+    while (padded.length < size) padded = `0${padded}`;
+    return padded;
+  }
+
+  return {
+    getOrders,
+    deleteOrder,
+    createOrder,
+    millisecondsToHuman,
+    pad
+  };
+}());
+
+class OrderList extends React.Component{
+  state = {
+    orders: []
+  };
+
+  componentDidMount(){
+    this.loadOrdersFromServer();
+    setInterval(this.loadOrdersFromServer, 5000);
+  }
+
+  loadOrdersFromServer = () => {
+    client.getOrders((serverOrders) => (
+      this.setState({ orders: serverOrders })
+      )
+    );
+  };
+
+  handleRemove = (orderId) => {
+    this.deleteOrder(orderId);
+  };
+
+  deleteOrder = (orderId) => {
+    this.setState({
+      orders: this.state.orders.filter(x => x.id !== orderId)
+    });
+    client.deleteOrder(
+      { id: orderId }
+    );
+  };
+
+  handleCreateFormSubmit = (order) => {
+    this.createOrder(order);
+  }
+
+  createOrder = (order) => {
+    const x = {
+      id: order.id,
+      item: order.item,
+      price: order.price,
+      table: order.table,
+      runningSince: Date.now()
+    };
+    this.setState({
+      orders: this.state.orders.concat(x)
+    });
+
+    client.createOrder(x);
+  }
+  render(){
+    const orders = this.state.orders.map((order) => (
+      <Order
+        key={order.id}
+        id={order.id}
+        item={order.item}
+        price={order.price}
+        table={order.table}
+        runningSince={order.runningSince}
+        onRemoveClick={this.handleRemove}
+      />
+    ));
+    return(
+      <div className='orders'>
+        {orders}
+        <ToggleableForm 
+          onFormSubmit={this.handleCreateFormSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+class Order extends React.Component{
+  handleRemoveClick = () => {
+    this.props.onRemoveClick(this.props.id);
+  };
+
+  render(){
+    return(
+      <div className='ui raised segment'>
+        <div className='ui medium header'>
+          Order #{this.props.id}
+        </div>
+        <div className='ui horizontal segments'>
+          <div className='ui segment'>
+            {this.props.item}
+          </div>
+          <div className='ui segment'>
+            {this.props.price} kn
+          </div>
+          <div className='ui segment'>
+            Table: {this.props.table}
+          </div>
+          <div className='ui segment'>
+            Waiting for: <Timer runningSince={this.props.runningSince} />
+          </div>
+        </div>
+        <div className='ui basic content center aligned segment'>
+          <button className='ui negative basic button' onClick={this.handleRemoveClick}>
+            Remove order
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+class Timer extends React.Component{
+  componentDidMount(){
+    this.forceUpdateInterval = setInterval(() => this.forceUpdate(), 50);
+  }
+
+  componentWillUnmount(){
+    clearInterval(this.forceUpdateInterval);
+  }
+
+  render(){
+    const delta = Date.now() - this.props.runningSince;
+    const elapsedString = client.millisecondsToHuman(delta);
+    return(
+      <span>
+        {elapsedString}
+      </span>
+    );
+  }
+}
+
+class ToggleableForm extends React.Component{
+  state = {
+    isOpen: false
+  };
+
+  handleFormOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
+  handleFormClose = () => {
+    this.setState({ isOpen: false });
+  };
+
+  handleFormSubmit = (order) => {
+    this.props.onFormSubmit(order);
+    this.setState({ isOpen: false });
+  };
+
+  render(){
+    if(this.state.isOpen){
+      return(
+        <OrderForm
+          onFormSubmit={this.handleFormSubmit}
+          onFormClose={this.handleFormClose}
+        />
+      );
+    } else {
+      return(
+        <div className='ui basic content center aligned segment'>
+            <button
+              className='ui basic button icon'
+              onClick={this.handleFormOpen}
+            >
+              <i className='plus icon' />
+            </button>
+          </div>
+      );
+    }
+  }
+}
+
+class OrderForm extends React.Component{
+  state = {
+    id: '',
+    item: '',
+    price: '',
+    table: ''
+  };
+
+  handleIdChange = (e) => {
+    this.setState({ id: parseInt(e.target.value, 10) });
+  };
+
+  handleItemChange = (e) => {
+    this.setState({ item: e.target.value });
+  };
+
+  handlePriceChange = (e) => {
+    this.setState({ price: parseFloat(e.target.value) });
+  };
+
+  handleTableChange = (e) => {
+    this.setState({ table: parseInt(e.target.value, 10) });
+  };
+
+  handleSubmit = () => {
+    this.props.onFormSubmit({
+      id: this.state.id,
+      item: this.state.item,
+      price: this.state.price,
+      table: this.state.table
+    });
+  };
+
+  render(){
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='ui form'>
+            <div className='field'>
+              <label>Order #</label>
+              <input
+                type='number'
+                value={this.state.id}
+                onChange={this.handleIdChange}
+              />
+            </div>
+            <div className='field'>
+              <label>Item</label>
+              <input
+                type='text'
+                value={this.state.item}
+                onChange={this.handleItemChange}
+              />
+            </div>
+            <div className='field'>
+              <label>Price</label>
+              <input
+                type='number'
+                value={this.state.price}
+                onChange={this.handlePriceChange}
+              />
+            </div>
+            <div className='field'>
+              <label>Table</label>
+              <input
+                type='number'
+                value={this.state.table}
+                onChange={this.handleTableChange}
+              />
+            </div>
+            <div className='ui two bottom attached buttons'>
+              <button
+                className='ui basic blue button'
+                onClick={this.handleSubmit}
+              >
+                Create
+              </button>
+              <button
+                className='ui basic red button'
+                onClick={this.props.onFormClose}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+/*
+ReactDOM.render(
+  <OrderList />,
+  document.getElementById('root')
+);
+*/
+
+export default OrderList;
---
+++ server.js
@@ -0,0 +1,122 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+
+const DATA_FILE = path.join(__dirname, 'data.json');
+
+app.set('port', (process.env.PORT || 3001));
+
+app.use('/', express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-cache, no-store, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
+
+app.get('/api/orders', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.post('/api/orders', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const orders = JSON.parse(data);
+    const newOrder = {
+      id: req.body.id,
+      item: req.body.item,
+      price: req.body.price,
+      table: req.body.table,
+      runningSince: Date.now()
+    };
+    orders.push(newOrder);
+    fs.writeFile(DATA_FILE, JSON.stringify(orders, null, 4), () => {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(orders);
+    });
+  });
+});
+
+app.post('/api/timers/start', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach((timer) => {
+      if (timer.id === req.body.id) {
+        timer.runningSince = req.body.start;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.post('/api/timers/stop', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach((timer) => {
+      if (timer.id === req.body.id) {
+        const delta = req.body.stop - timer.runningSince;
+        timer.elapsed += delta;
+        timer.runningSince = null;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.put('/api/timers', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    const timers = JSON.parse(data);
+    timers.forEach((timer) => {
+      if (timer.id === req.body.id) {
+        timer.title = req.body.title;
+        timer.project = req.body.project;
+      }
+    });
+    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.delete('/api/orders', (req, res) => {
+  fs.readFile(DATA_FILE, (err, data) => {
+    let orders = JSON.parse(data);
+    orders = orders.reduce((memo, order) => {
+      if (order.id === req.body.id) {
+        return memo;
+      } else {
+        return memo.concat(order);
+      }
+    }, []);
+    fs.writeFile(DATA_FILE, JSON.stringify(orders, null, 4), () => {
+      res.json({});
+    });
+  });
+});
+
+app.get('/molasses', (_, res) => {
+  setTimeout(() => {
+    res.end();
+  }, 5000);
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`Find the server at: http://localhost:${app.get('port')}/`); // eslint-disable-line no-console
+});
