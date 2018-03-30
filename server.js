6ef50e198b8ee3a5644f0d28b97359490e054a81
Petar Susac <petar.susac@gmail.com>
2018-03-30 15:04:40 +0200

    pocistio server

--- server.js
+++ server.js
@@ -61,51 +61,6 @@ app.get('/api/menu', (req, res) => {
   res.json(menu);
 })
 
-app.post('/api/timers/start', (req, res) => {
-  fs.readFile(DATA_FILE, (err, data) => {
-    const timers = JSON.parse(data);
-    timers.forEach((timer) => {
-      if (timer.id === req.body.id) {
-        timer.runningSince = req.body.start;
-      }
-    });
-    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
-      res.json({});
-    });
-  });
-});
-
-app.post('/api/timers/stop', (req, res) => {
-  fs.readFile(DATA_FILE, (err, data) => {
-    const timers = JSON.parse(data);
-    timers.forEach((timer) => {
-      if (timer.id === req.body.id) {
-        const delta = req.body.stop - timer.runningSince;
-        timer.elapsed += delta;
-        timer.runningSince = null;
-      }
-    });
-    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
-      res.json({});
-    });
-  });
-});
-
-app.put('/api/timers', (req, res) => {
-  fs.readFile(DATA_FILE, (err, data) => {
-    const timers = JSON.parse(data);
-    timers.forEach((timer) => {
-      if (timer.id === req.body.id) {
-        timer.title = req.body.title;
-        timer.project = req.body.project;
-      }
-    });
-    fs.writeFile(DATA_FILE, JSON.stringify(timers, null, 4), () => {
-      res.json({});
-    });
-  });
-});
-
 app.delete('/api/orders', (req, res) => {
   fs.readFile(DATA_FILE, (err, data) => {
     let orders = JSON.parse(data);
